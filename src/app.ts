968ca765a050182260cb94d412236ad30784f3b2
andrian.moroi <andrian.moroi@hotmail.com>
2020-11-29 16:10:59 +0200

    added html pages

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("it's working")
+import * as bodyParser from "body-parser";
+import express from 'express';
+
+let port = 3000
+let app: express.Application = express()
+let router = express.Router()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get("/api/test", (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(port, () => {
+    console.log("listen to port " + port)
+})
