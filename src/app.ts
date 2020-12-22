338b8a4e4d17400e0f2c023cef0e33b04339e6ea
strelok91 <andrian.moroi@gmail.com>
2020-12-22 21:28:18 +0200

    added support for multiple channels stored in channels.json

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,51 @@
 import * as bodyParser from "body-parser";
+import { exec, execSync } from 'child_process';
 import express from 'express';
 
 let port = 3000
 let app: express.Application = express()
 let router = express.Router()
 
-app.use(bodyParser.json())
+app.use(express.json());
+// app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get("/api/test", (req, res) => {
     res.send("Hello world")
 })
 
+app.get("/api/getAllChannels", (req, res) => {
+    res.sendFile(`${process.cwd()}/sources/channels.json`)
+})
+
+app.post("/api/start", (req, res) => {
+    stopPlayingVideo();
+
+    let url = req.body.url;
+    let runCommand = `python3 tools/player.py \"${url}\"`;
+
+    exec(runCommand, (error, response) => {
+        console.log(error);
+        console.log(response);
+    });
+
+    res.sendStatus(200)
+})
+
+app.post("/api/stop", (req, res) => {
+    stopPlayingVideo();
+
+    res.sendStatus(200)
+})
+
 app.listen(port, () => {
     console.log("listen to port " + port)
 })
+
+function stopPlayingVideo() {
+    let pidsToStop = "ps -aux | egrep \"[t]ools/player\.py|[s]treamlink|[o]mxplayer\" | grep -Po \"pi *?\\d+\" | grep -Po \"\\d+\" | xargs --no-run-if-empty kill";
+
+    execSync(pidsToStop)
+
+    console.log('stop playing')
+}
---
+++ tools/player.py
@@ -0,0 +1,14 @@
+#import subprocess, os, time
+import sys, tempfile, os, os.path, shutil, subprocess, time, requests
+
+if len(sys.argv) < 2:
+    raise Exception("The first argument must be the stream url")
+
+url = sys.argv[1]
+
+while True:
+    try:
+        print("started new process")
+        os.system("streamlink --player \"omxplayer -o hdmi --threshold 0.5\" \"" + url + "\" best --player-fifo")
+    except:
+        pass
